4f059e02ff235ae397befcfcba06cc274bac36ec
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2015-10-26 14:47:49 -0400

    - missing files
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

---
+++ configuration/configurator/JSONMarkUp.cpp
@@ -0,0 +1,71 @@
+#include "jstring.hpp"
+#include "SchemaCommon.hpp"
+#include "BuildSet.hpp"
+#include "JSONMarkUp.hpp"
+#include "ConfigSchemaHelper.hpp"
+
+using namespace CONFIGURATOR;
+
+void CJSONMarkUpHelper::createUIContent(::StringBuffer &strJSON, unsigned int &offset, const char *pUIType, const char* pLabel, const char* pKey, \
+                                        const char *pToolTip, const char *pDefaultValue, const char *pValues)
+{
+    assert(pUIType);
+
+    offset+= STANDARD_OFFSET_1;
+
+    strJSON.appendf(" %s \"%s\",", JSON_LABEL, pLabel);
+    strJSON.appendf(" %s \"%s\",", JSON_TYPE, pUIType);
+
+    //strJSON.appendf(" %s \"%s\",", JSON_KEY, pKey);
+    StringBuffer strKey(pKey);
+    strKey.replace('/','_'); // for frontend
+    strJSON.appendf(" %s \"%s\",", JSON_KEY, strKey.str());
+
+    strJSON.appendf(" %s \"%s\",", JSON_TOOLTIP, pToolTip);
+    strJSON.appendf(" %s \"%s\"", JSON_DEFAULT_VALUE, pDefaultValue);
+
+    if (strcmp(pUIType, JSON_TYPE_DROP_DOWN) == 0 && pValues && *pValues)
+    {
+        strJSON.appendf(", %s [ \"%s\" ]", JSON_VALUES, pValues);
+    }
+    else if (strcmp(pUIType, JSON_TYPE_TABLE) == 0 && pValues && *pValues)
+    {
+        strJSON.appendf(", %s [ \"%s\" ]", JSON_COLUMN_NAMES_VALUE, pValues);
+    }
+}
+
+void CJSONMarkUpHelper::getNavigatorJSON(::StringBuffer &strJSON)
+{
+    int nComponents = CBuildSetManager::getInstance()->getBuildSetComponentCount();
+
+    if (nComponents == 0)
+        return;
+
+    strJSON.append(JSON_NAVIGATOR_BEGIN);
+    strJSON.append(JSON_NAVIGATOR_TEXT).append("\"").append(CConfigSchemaHelper::getInstance()->getEnvFilePath()).append("\",").append("\n");
+    strJSON.append(JSON_NAVIGATOR_KEY).append("\"").append(CConfigSchemaHelper::getInstance()->getEnvFilePath()).append("\",").append("\n");
+
+    for (int i = 0; i < nComponents; i++)
+    {
+        if (i == 0)
+        {
+            strJSON.append("\n");
+            strJSON.append(JSON_NAVIGATOR_NODES);
+            strJSON.append(JSON_NAVIGATOR_NODE_BEGIN);
+        }
+        strJSON.append(JSON_NAVIGATOR_TEXT).append("\"").append(CBuildSetManager::getInstance()->getBuildSetComponentName(i)).append("\",").append("\n");
+        strJSON.append(JSON_NAVIGATOR_KEY).append("\"").append(CBuildSetManager::getInstance()->getBuildSetComponentFileName(i)).append("\"");
+
+        if (i+1 < nComponents)
+        {
+            strJSON.append("},\n{");
+        }
+        else
+        {
+            strJSON.append("\n");
+            strJSON.append(JSON_NAVIGATOR_NODE_END);
+        }
+
+    }
+    strJSON.append(JSON_NAVIGATOR_END);
+}
