7f8052320dd0d52804dbbabde03f3e89001555f3
Gleb Aronsky <gleb.aronsky@lexisnexis.com>
2015-12-07 13:51:28 -0500

    - fixup json - fixup navigator
    Signed-off-by: Gleb Aronsky <gleb.aronsky@lexisnexis.com>

--- configuration/configurator/JSONMarkUp.hpp
+++ configuration/configurator/JSONMarkUp.hpp
@@ -67,8 +67,8 @@ class CJSONMarkUpHelper
 {
 
 public:
-    //static void createUIContent(::StringBuffer &strJSON, unsigned int &offset, const char *pUIType, const char* pLabel, const char* pKey, const char *pToolTip = "", const char *pDefaultValue = "", const char* pValues = "", const char*  pValue = "");
-    static void createUIContent(::StringBuffer &strJSON, unsigned int &offset, ::StringBuffer strUIType, ::StringBuffer strLabel, ::StringBuffer strKey, ::StringBuffer strToolTip = "", ::StringBuffer strDefaultValue = "", ::StringBuffer strValues = "", ::StringBuffer strValue = "");
+    static void createUIContent(::StringBuffer &strJSON, unsigned int &offset, const char *pUIType, const char* pLabel, const char* pKey, const char *pToolTip = "", const char *pDefaultValue = "", const char* pValues = "", const char*  pValue = "");
+    //static void createUIContent(::StringBuffer &strJSON, unsigned int &offset, ::StringBuffer strUIType, ::StringBuffer strLabel, ::StringBuffer strKey, ::StringBuffer strToolTip = "", ::StringBuffer strDefaultValue = "", ::StringBuffer strValues = "", ::StringBuffer strValue = "");
     static void getNavigatorJSON(::StringBuffer &strJSON);
 private:
     static void markUpString(::StringBuffer &str);
